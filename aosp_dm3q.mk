5e6806de7d6e30bc08a573e9f61e3cda40860295
Machad3x <machad3x@gmail.com>
2025-06-24 10:15:16 -0400

    dm3q: Update from S918BXXS8DYF1

--- aosp_dm3q.mk
+++ aosp_dm3q.mk
@@ -23,7 +23,7 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="dm3qxxx-user 15 AP3A.240905.015.A2 S918BXXS8DYDB release-keys" \
-    BuildFingerprint=samsung/dm3qxxx/dm3q:15/AP3A.240905.015.A2/S918BXXS8DYDB:user/release-keys \
+    BuildDesc="dm3qxxx-user 15 AP3A.240905.015.A2 S918BXXS8DYF1 release-keys" \
+    BuildFingerprint=samsung/dm3qxxx/dm3q:15/AP3A.240905.015.A2/S918BXXS8DYF1:user/release-keys \
     DeviceProduct=dm3qxxx \
     SystemName=dm3qxxx
